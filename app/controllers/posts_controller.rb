2ff0922a1de54a775c3396a89679d9de51e50a30
vagrant <vagrant@rails-dev-box>
2014-11-19 02:35:10 +0000

    will-paginatie

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,12 +31,13 @@ class PostsController < ApplicationController
     end
   end
 
+   
   def update
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(post_params)
+    @post = Post.find(params[:id])
     authorize @post
 
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-   belongs_to :topic 
+  belongs_to :topic
 
-   default_scope { order('created_at DESC') }
+  default_scope { order('created_at DESC') }
 
-    validates :title, length: { minimum: 5 }, presence: true
-   validates :body, length: { minimum: 20 }, presence: true
-   validates :topic, presence: true
-   validates :user, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
-   mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 end
