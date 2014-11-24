6bd53142f43d045305de034c5246ca4245a2e43f
Worldofwaffle <drekslpr@gmail.com>
2014-11-24 09:03:24 +0000

    destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,19 @@ class CommentsController < ApplicationController
       redirect_to [@topic, @post], notice: "Comment failed to save."
     end
   end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    authorize @comment
+
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
 
 
   private
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,7 +7,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
+  # storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -25,31 +25,30 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  #process :resize_to_fit => [200, 300]
+  # process :scale => [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-    version :tiny do
-     process resize_to_fill: [20, 20]
-   end
- 
-   version :small do
-     process resize_to_fill: [30, 30]
-   end
- 
-   version :profile do
-     process resize_to_fill: [45, 45]
-   end
+  version :tiny do
+    process resize_to_fill: [20, 20]
+  end
 
+  version :small do
+    process resize_to_fill: [30, 30]
+  end
+
+  version :profile do
+    process resize_to_fill: [45, 45]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
-     %w(jpg jpeg gif png)
-  end
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
