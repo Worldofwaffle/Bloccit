e2b325258b963ed27cbf2c38cacc8c9a024a8362
Worldofwaffle <drekslpr@gmail.com>
2014-12-04 03:12:44 +0000

    broken-votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Bloccit::Application.routes.draw do
 
-  devise_for :users
+   devise_for :users
   resources :users
     
-  resources :topics do
+   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
-    end
-  end
 
-  get 'about', to: 'welcome#about'
+     post '/up-vote' => 'votes#up_vote', as: :up_vote
+     post '/down-vote' => 'votes#down_vote', as: :down_vote
+  end
+end
+     
+
+ 
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
-  
