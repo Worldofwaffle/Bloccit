bf9b11c49b3323b237b985583277fb8207cba0f2
Worldofwaffle <drekslpr@gmail.com>
2014-12-10 03:01:43 +0000

    mocking with rspec

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,7 @@ users = User.all
      body:   Faker::Lorem.paragraph
    )
    post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+   post.create_vote
    post.update_rank
  end
  posts = Post.all
