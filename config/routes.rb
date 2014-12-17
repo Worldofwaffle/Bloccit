b7f003c285c32517871598ba02000c2b9b6c0bb8
Worldofwaffle <drekslpr@gmail.com>
2014-12-17 03:48:42 +0000

    popular_posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
- Bloccit::Application.routes.draw do
+Bloccit::Application.routes.draw do
 
   devise_for :users
-  resources :users
+  resources :users, only: [:update, :show, :index]
     
-   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-       resources :favorites, only: [:create, :destroy]
-    end
-   end
-   resources :posts, only: [:index] do
-     
-   end
-   
-  post '/up-vote' => 'votes#up_vote', as: :up_vote
-  post '/down-vote' => 'votes#down_vote', as: :down_vote
- 
+  resources :topics do
+    resources :posts, except: [:index], controller: 'topics/posts'
+  end
+
+  resources :posts, only: [:index] do
+    resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
+  end
+
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
+
